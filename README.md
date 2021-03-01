0e6fce5c2e10739ab3475ca901789df3b70e2346
Bateman <66702639+SelfAdjointOperator@users.noreply.github.com>
2021-03-01 19:19:11 +0000

    first main commit

--- README.md
+++ README.md
@@ -1,7 +1,35 @@
-# SAO Template
+# 2b2tPlace-DiscordBot
 
-This is my template repository
+This repo contains the source code of the Discord bot I use for [2b2t.Place](https://2b2t.Place)
 
-## H2 Headings like this
+The bot will respond to messages in the channel with id specified in `bot/botConfig_public.json` beginning with `'!token'`. The bot will then try to DM the message author either their token for the website, or will inform them that they can only choose a pixel once per day. Errors are handled and logged in the `bot/logs/errors` folder.
 
-- Bullet points
+## Setup
+Virtual environment uses:
+
+- `pip install requests`
+- `pip install discord.py`
+
+## Config
+
+The two config files `bot/botConfig_public.json` and `bot/botConfig_private.json` are loaded. The latter contains sensitive / private information that should only be available to the web admin. The file structures are as follows:
+
+```
+botConfig_public.json
+{
+    "channelId": {
+        "getToken": id of Discord channel that the bot should respond to messages in
+    },
+    "APIEndpoint": {
+        "tokenJSON": url to 2b2t.place token api
+    }
+}
+```
+
+```
+botConfig_private.json
+{
+    "discordBotToken": token given by Discord to log into the bot,
+    "websiteAPIAuthKey": private authKey that is kept a secret between the website API and Discord bot
+}
+```
---
+++ bot/__init__.py
@@ -0,0 +1,113 @@
+import os
+import json
+import time
+import random
+import aiohttp
+import discord
+import datetime
+# from discord.ext import commands
+
+client = discord.Client()
+
+GLOBAL_BOTCONFIG_PRIVATE = None # loaded in if __name__
+GLOBAL_BOTCONFIG_PUBLIC  = None # loaded in if __name__
+
+################################################################################
+
+class ManualLogging():
+    """For logging 401 and 403 errors etc"""
+
+    filepathBase_error = "./logs/errors/error_{}.json"
+
+    @staticmethod
+    def saveJSON(filepath, pythonJSONObject):
+        filepath = os.path.normpath(filepath)
+        os.makedirs(os.path.split(filepath)[0], exist_ok = True)
+        with open(filepath, "w+") as file:
+            file.write(json.dumps(pythonJSONObject, indent = 4))
+
+    @staticmethod
+    def logErrorJSON(pythonJSONObject, addTimestampIfNotPresent = True):
+        timestamp = int(time.time())
+        if addTimestampIfNotPresent and not "timestamp" in pythonJSONObject:
+            pythonJSONObject["timestamp"] = timestamp
+        randomSuffix = "_%08x" % random.randrange(16 ** 8) # incase several errors happen at once
+        filepath = ManualLogging.filepathBase_error.format(str(timestamp) + randomSuffix)
+        ManualLogging.saveJSON(filepath, pythonJSONObject)
+
+################################################################################
+
+@client.event
+async def on_ready():
+    timestamp = int(time.time())
+    print("Logged in as {} at {}".format(client.user, str(timestamp)))
+
+@client.event
+async def on_message(message):
+    message_author = message.author
+    if  message_author == client.user or \
+        message.channel.id != GLOBAL_BOTCONFIG_PUBLIC["channelId"]["getToken"] or \
+        not message.content.startswith("!token"):
+            return
+    try:
+        await message_author.send("Getting token... 🤖🗺️🎟️")
+    except discord.Forbidden:
+        await message.channel.send("{} please allow DMs from me, the bot! 🥺".format(message_author.mention))
+        return
+    try:
+        headers = {
+            "authKey": GLOBAL_BOTCONFIG_PRIVATE["websiteAPIAuthKey"],
+            "discordUUID": (discordUUID := str(message_author.id)),
+            "discordTag": (discordTag := "{}#{}".format(message_author.name, message_author.discriminator))
+        }
+        async with aiohttp.ClientSession(headers = headers) as session:
+            async with session.get(GLOBAL_BOTCONFIG_PUBLIC["APIEndpoint"]["tokenJSON"]) as r:
+                if (statusCode := r.status) != 200:
+                    errorDict = {
+                        "statusCode":  statusCode,
+                        "discordUUID": discordUUID,
+                        "discordTag":  discordTag
+                    }
+                    ManualLogging.logErrorJSON(errorDict)
+                    await message_author.send("Bot incurred status code {} while trying to connect to 2b2t.Place!\nError has been logged and will be fixed soon, apologies! 😵".format(str(statusCode)))
+                else:
+                    r_json = await r.json()
+                    if "nextTimeAllowed" in r_json:
+                        await message_author.send("You may only choose a pixel once per day! 😏\nNext token available at {} UTC".format(datetime.datetime.utcfromtimestamp(int(r_json["nextTimeAllowed"])).strftime("%Y-%m-%d %H:%M:%S")))
+                    elif "token" in r_json:
+                        await message_author.send("Your token is: '{}' 🎟️".format(r_json["token"]))
+                    else:
+                        errorDict = {
+                            "statusCode":  "BAD_API_RESPONSE",
+                            "discordUUID": discordUUID,
+                            "discordTag":  discordTag,
+                            "APIResponse": r_json
+                        }
+                        ManualLogging.logErrorJSON(errorDict)
+                        await message_author.send("Bot received unexpected API response from 2b2t.Place!\nError has been logged and will be fixed soon, apologies! 😵")
+    except aiohttp.client_exceptions.ClientConnectorError:
+        errorDict = {
+            "statusCode":  "FAILED_TO_CONNECT",
+            "discordUUID": discordUUID,
+            "discordTag":  discordTag
+        }
+        ManualLogging.logErrorJSON(errorDict)
+        await message_author.send("Bot failed to connect to 2b2t.Place!\nError has been logged and will be fixed soon, apologies! 😵")
+
+################################################################################
+
+if __name__ == "__main__":
+    try:
+        with open("./botConfig_private.json") as botConfig_private_file:
+            GLOBAL_BOTCONFIG_PRIVATE = json.load(botConfig_private_file)
+    except FileNotFoundError:
+        print("Error: File './botConfig_private.json' not found")
+        raise SystemExit
+    try:
+        with open("./botConfig_public.json") as botConfig_public_file:
+            GLOBAL_BOTCONFIG_PUBLIC = json.load(botConfig_public_file)
+    except FileNotFoundError:
+        print("Error: File './botConfig_public.json' not found")
+        raise SystemExit
+
+    client.run(GLOBAL_BOTCONFIG_PRIVATE["discordBotToken"])
